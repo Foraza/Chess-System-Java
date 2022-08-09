f784be0ad2e78e38768a0c1c3eb332f45bcf5def
Foraza <gabrielfogassa@gmail.com>
2022-08-09 00:16:48 -0300

    Added the chess layer and Piece, Board and Position classes, besides prints the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,20 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+	
+	private Colors color;
+
+	public ChessPiece(Board board, Colors color) {
+		super(board);
+		this.color = color;
+	}
+	
+	public Colors getColor() {
+		return this.color;
+	}
+	
+	
+}
